34b43ef72fd6a47364add5dc046ab6ed26c68c4a
Luthere15 <42876725+Luthere15@users.noreply.github.com>
2019-03-19 21:04:03 -0400

    level and player created

---
+++ demon slayer/Assets/scripts/followCam.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followCam : MonoBehaviour
+{
+    static public GameObject POI;
+    public float camY;
+    [SerializeField]
+    private float xMax;
+    [SerializeField]
+    private float yMax;
+    [SerializeField]
+    private float xMin;
+    [SerializeField]
+    private float yMin;
+
+    private Transform target;
+
+    void Awake()
+    {
+        camY = this.transform.position.y;
+    }
+
+    void FixedUpdate()
+    {
+        if (POI == null) return;
+
+        Vector2 destination = POI.transform.position;
+
+        destination.x = camY;
+
+        transform.position = destination;
+    }
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(Mathf.Clamp(target.position.x, xMin, xMax), Mathf.Clamp(target.position.y, yMin, yMax), transform.position.z);
+
+    }
+}
---
+++ demon slayer/Assets/scripts/player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private Vector2 movement;
+    public float speed;
+    private Rigidbody2D rb;
+
+    public float jumpForce;
+    public bool isjumping;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        float moveHorizontal = Input.GetAxis("Horizontal") * speed;
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+        rb.AddForce(movement);
+        Rigidbody2D constraints;
+
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") * speed;
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+        rb.AddForce(movement);
+
+        jump();
+    }
+
+    void jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isjumping)
+        {
+            isjumping = true;
+
+            rb.AddForce(new Vector2(movement.x, jumpForce));
+        }
+    }
+}
